db9f84d6e761255daa5a825176af8b550a9ba10e
sgtchilipapi <jayemmanuelg@gmail.com>
2023-02-26 11:35:21 +0800

    feat: add premium ankr endpoint

--- app-config/networks.js
+++ app-config/networks.js
@@ -1,6 +1,6 @@
 const fantom_mainnet = {
-    http:"https://rpc.ankr.com/fantom/36fdae799ada936183156f53af23bd097751d19137e62e1b9c03f8ec8793395a",
-    wss:"wss://rpc.ankr.com/fantom/ws/36fdae799ada936183156f53af23bd097751d19137e62e1b9c03f8ec8793395a"
+    http:"https://rpc.ankr.com/fantom/f4901f732bb5c937b83bab515d0cd80fbd5b8892f247debe56a35a360ad6c864",
+    wss:"wss://rpc.ankr.com/fantom/ws/f4901f732bb5c937b83bab515d0cd80fbd5b8892f247debe56a35a360ad6c864"
 }
 
 module.exports = {
