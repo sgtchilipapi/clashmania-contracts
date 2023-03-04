eb6de2f28e6c470571fa2cfbcee2e62d6a8236f7
sgtchilipapi <jayemmanuelg@gmail.com>
2023-03-04 11:17:30 +0800

    feat: add vrf constructor args

--- app-config/deployments.js
+++ app-config/deployments.js
@@ -1,247 +1,139 @@
 const mainnet_deployments = {
     tokens: {
         wftm: {
-            address: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83"
+            address: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+            abi: "https://ftmscan.com/address/0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83#code"
         },
         clank: {
             address: "0xA13473ffb118655cdF6F15684Ee6CCE4E4aeaf26",
-            abi: ""
+            abi: "https://ftmscan.com/address/0xA13473ffb118655cdF6F15684Ee6CCE4E4aeaf26#code"
         },
         boom: {
             address: "0x5751141f10e9cCC97f7f542253F67AC6C67ef308",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x5751141f10e9cCC97f7f542253F67AC6C67ef308#code"
         },
         thump: {
             address: "0x7F11f903EE76aaF287091509E7b38A877a5500CF",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x7F11f903EE76aaF287091509E7b38A877a5500CF#code"
         },
         clink: {
             address: "0x968F94214F1b5F4C775e7F7a04ca2FAE6E545E75",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x968F94214F1b5F4C775e7F7a04ca2FAE6E545E75#code"
         },
         snap: {
             address: "0x31010F5C1444Ba33eD38E4A40e57742829c04BA3",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x31010F5C1444Ba33eD38E4A40e57742829c04BA3#code"
         },
         yellowspark: {
             address: "0xb22634b9C9B4363E0519c8aDC07632aE87CAFcf6",
-            abi: ""
+            abi: "https://ftmscan.com/address/0xb22634b9C9B4363E0519c8aDC07632aE87CAFcf6#code"
         },
         whitespark: {
             address: "0x2b2C1daBa39c2ba4681aAB86226F6C72c6369f2E",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x2b2C1daBa39c2ba4681aAB86226F6C72c6369f2E#code"
         },
         redspark: {
             address: "0x13f24AD7BF6E83c6244BB0666ECd6ee38d40E7cD",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x13f24AD7BF6E83c6244BB0666ECd6ee38d40E7cD#code"
         },
         bluespark: {
             address: "0x7EA3C6BF16D858de0615e8524399Ed25ADF3b7bD",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x7EA3C6BF16D858de0615e8524399Ed25ADF3b7bD#code"
         },
         enerlink: {
             address: "0x50207C81d58A3429Ee5E3f2cE437cbF09545C2F6",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x50207C81d58A3429Ee5E3f2cE437cbF09545C2F6#code"
         },
         clankftm: {
-            address: "0x8F417a08B1981256E5C5ae59b1E62C2bBb273c64"
+            address: "0x8F417a08B1981256E5C5ae59b1E62C2bBb273c64",
+            abi: "https://ftmscan.com/address/0x8F417a08B1981256E5C5ae59b1E62C2bBb273c64#code"
         },
         clankboom: {
-            address: "0xac31D24F278336a15240Eef4d0933E5170f0f7e8"
+            address: "0xac31D24F278336a15240Eef4d0933E5170f0f7e8",
+            abi: "https://ftmscan.com/address/0xac31D24F278336a15240Eef4d0933E5170f0f7e8#code"
         },
         clankthump: {
-            address: "0x5B6448f622f77ff2FF099554E968798DbFf29435"
+            address: "0x5B6448f622f77ff2FF099554E968798DbFf29435",
+            abi: "https://ftmscan.com/address/0x5B6448f622f77ff2FF099554E968798DbFf29435#code"
         },
         clankclink: {
-            address: "0xFbe4Ad14cBe23ba11d2f5846152Bc7dd8F993f3c"
+            address: "0xFbe4Ad14cBe23ba11d2f5846152Bc7dd8F993f3c",
+            abi: "https://ftmscan.com/address/0xFbe4Ad14cBe23ba11d2f5846152Bc7dd8F993f3c#code"
         },
         clanksnap: {
-            address: "0x7CA89dfcFee4545DFe1CA399EFd639b310Ca7f9F"
+            address: "0x7CA89dfcFee4545DFe1CA399EFd639b310Ca7f9F",
+            abi: "https://ftmscan.com/address/0x7CA89dfcFee4545DFe1CA399EFd639b310Ca7f9F#code"
         }
         
     },
     defi: {
         minichefv2: {
             address: "0x5e82414A539C46E9445750D14A532eeBb14fFA1c",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x5e82414A539C46E9445750D14A532eeBb14fFA1c#code"
         },
         factory: {
             address: "0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3",
-            abi: ""
+            abi: "https://ftmscan.com/address/0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3#code"
         }
     },
     characters: {
         nftContract: {
             address: "0x4ecbb337eECd76a2f1D536D85949d122aA89bFfa",
-            abi: ''
+            abi: 'https://ftmscan.com/address/0x4ecbb337eECd76a2f1D536D85949d122aA89bFfa#code'
         },
         minter: {
             address: "0x45E678Fc5c784F0d58aAC7a1624d8F2571186F34",
-            abi: ''
+            abi: 'https://ftmscan.com/address/0x45E678Fc5c784F0d58aAC7a1624d8F2571186F34#code'
         },
         vrf: {
             address: "0x0EaC5B658EBf5199A418cdA0f6a7c8D17472d46f",
-            abi: ''
+            abi: 'https://ftmscan.com/address/0x0EaC5B658EBf5199A418cdA0f6a7c8D17472d46f#code'
         },
         uriConstructor: {
-            address: "0x1A5ec459e68B84bf0D87dcC6B13b36878B91945d"
+            address: "0x1A5ec459e68B84bf0D87dcC6B13b36878B91945d",
+            abi: "https://ftmscan.com/address/0x1A5ec459e68B84bf0D87dcC6B13b36878B91945d#code"
         }
     },
     equipments: {
         nftContract: {
             address: "0xab5F3de7793B8E65dB53D677F5B712079Cf4fee0",
-            abi: ''
+            abi: 'https://ftmscan.com/address/0xab5F3de7793B8E65dB53D677F5B712079Cf4fee0#code'
         },
         minter: {
             address: "0x167a1EE7F30aEfA30aD16558557Cc5FF31B4A611",
-            abi: ''
+            abi: 'https://ftmscan.com/address/0x167a1EE7F30aEfA30aD16558557Cc5FF31B4A611#code'
         },
         vrf: {
             address: "0x0C8520cda7C4d5cF0f4ED71f95E9276E95fE9998",
-            abi: ''
+            abi: 'https://ftmscan.com/address/0x0C8520cda7C4d5cF0f4ED71f95E9276E95fE9998#code'
         },
         manager: {
             address: "0x497b67F1a0ecfAA41bB5350fBaC20E11267b77f0",
-            abi: ''
+            abi: 'https://ftmscan.com/address/0x497b67F1a0ecfAA41bB5350fBaC20E11267b77f0#code'
         }
     },
     dungeons: {
         dungeon: {
             address: "0x06a5860B2713A6198F187D4e59b0eE3a8b5E12dD",
+            abi: "https://ftmscan.com/address/0x06a5860B2713A6198F187D4e59b0eE3a8b5E12dD#code"
         },
         vrf: {
-            address: "0x939eDd8812DDC138C12b515Fb415f0E22D5D4429"
+            address: "0x939eDd8812DDC138C12b515Fb415f0E22D5D4429",
+            abi: "https://ftmscan.com/address/0x939eDd8812DDC138C12b515Fb415f0E22D5D4429#code"
         },
         keeper: {
             registry: "0x02777053d6764996e594c3E88AF1D58D5363a2e6",
-            address: "0xd484de38aC5e3Ad3325f5F293B03D59aBaEe34A7"
+            address: "0xd484de38aC5e3Ad3325f5F293B03D59aBaEe34A7",
+            abi: "https://ftmscan.com/address/0xd484de38aC5e3Ad3325f5F293B03D59aBaEe34A7#code"
             ///Register new upkeep in https://automation.chain.link
         }
+    },
+    vrfconfig:{
+        subscription: 56,
+        coordinator: "0xd5D517aBE5cF79B7e95eC98dB0f0277788aFF634",
+        keyhash: "0x64ae04e5dba58bc08ba2d53eb33fe95bf71f5002789692fe78fb3778f16121c9",
     }
 }
-
-const testnet_deployments = {
-    tokens: {
-        wftm: {
-            address: "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889"
-        },
-        clank: {
-            address: "0x27f26ca1bCa51A232226013e59eBE51daADd267e",
-            abi: ""
-        },
-        boom: {
-            address: "0xbfd0E811189f609532A7eb8888B226c7d22365a2",
-            abi: ""
-        },
-        thump: {
-            address: "0xC3997c1DC8Ee58Be0Adf62259D0f891E642c43bf",
-            abi: ""
-        },
-        clink: {
-            address: "0x5f3887d4Faf942AfbD9EAAAD369E4c923E7ee30b",
-            abi: ""
-        },
-        snap: {
-            address: "0x914fC2727BC0260D0B744981920eBf7cc240be41",
-            abi: ""
-        },
-        yellowspark: {
-            address: "0x23Dc2720104066f7996521ef45DACD707205382a",
-            abi: ""
-        },
-        whitespark: {
-            address: "0xefFCF0D89F7C527aB72e852401382Be940215A89",
-            abi: ""
-        },
-        redspark: {
-            address: "0x26d5Bd493DE05c366491c2D5c3a2eD2C4F311cDa",
-            abi: ""
-        },
-        bluespark: {
-            address: "0xf3C3a29A02745DdF49e9dbF0797c8a81bbFD9C2d",
-            abi: ""
-        },
-        enerlink: {
-            address: "0x36CDf641a165bBa044946C5759B235bd88544B09",
-            abi: ""
-        },
-        clankftm: {
-            address: "0xA7A941341295E900D57A1ea36D271F18b11cfeA6"
-        },
-        clankboom: {
-            address: "0x633b433DC825BaEBe3Db8483648208AA96A240f5"
-        },
-        clankthump: {
-            address: "0xFDF6F1b765e8c26c9cf5979bE1FA7E2Aee79709D"
-        },
-        clankclink: {
-            address: "0xC40c3fc7125902dAb302044319428dED83736616"
-        },
-        clanksnap: {
-            address: "0x175498C283cCA7ef0bca525a9B5aa831B67c1c68"
-        }
-        
-    },
-    defi: {
-        minichefv2: {
-            address: "0x22ce9F8fd8694A097B9A712448f0773Fd67A8d19",
-            abi: ""
-        },
-        factory: {
-            address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
-            abi: ""
-        }
-    },
-    characters: {
-        nftContract: {
-            address: "0xF86B63d0F3dA33385dE1944DbBa3eA445Fd47E09",
-            abi: ''
-        },
-        minter: {
-            address: "0x3699A6dAD495350e72F589c47Ce8D258D1b6ea09",
-            abi: ''
-        },
-        vrf: {
-            address: "0x0517c405F8196ed2BB19fa9d90Ee34709Bbe8a09",
-            abi: ''
-        },
-        uriConstructor: {
-            address: "0x0Df9234C6B6f0F086d8514dd767592F233ae9B8d"
-        }
-    },
-    equipments: {
-        nftContract: {
-            address: "0x1F5CD9838a596B5C3aB5Aa913ae8Ac49312894F7",
-            abi: ''
-        },
-        minter: {
-            address: "0xD4Cb3CcB1714eb88593abF8fa76892d2aA964612",
-            abi: ''
-        },
-        vrf: {
-            address: "0x99d791A7F1867C305570233213C02Df677A498d6",
-            abi: ''
-        },
-        manager: {
-            address: "0x6f01d2e361602afa6694368A80B4b3aFb0f8E85D",
-            abi: ''
-        }
-    },
-    dungeons: {
-        dungeon: {
-            address: "0x8F0A642C98C572D9a4E3b6b4f1a31D257A6FB5EF",
-        },
-        vrf: {
-            address: "0x71414B16AE0AFC955CC44Cc53ed96FC135940c70"
-        },
-        keeper: {
-            address: "0x3823Fc4bB3a6B2Aa0A48b21A4bedd71f20e5b623"
-            ///Register new upkeep in https://automation.chain.link
-        }
-    }
-}
-
-
 
 const subgraph_deployments = {
     core: {
